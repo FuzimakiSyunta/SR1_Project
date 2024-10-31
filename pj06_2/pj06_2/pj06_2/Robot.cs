a53fd856c58193a141b0e264e21d78926c7d742b
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-10-31 14:35:06 +0900

    プロジェクト ファイルを追加します。

---
+++ pj06_2/pj06_2/pj06_2/Robot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Robot
+{
+	//フィールド
+	protected string name;
+
+
+	public Robot(string name)
+	{
+		this.name = name;
+	}
+
+	//nameの値返し
+	public string Getname()
+	{
+		return name;
+	}
+	//攻撃する
+    public void Attack()
+    {
+        Console.WriteLine("{0}は、攻撃した!", name);
+    }
+}
---
+++ pj06_2/pj06_2/pj06_2/TankRobot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace pj06_2
+{
+    internal class TankRobot : Robot
+    {
+        public TankRobot(string name) : base(name)
+        {
+        }
+
+
+        //キャノン砲を撃つ
+        public void Attack()
+        {
+            Console.WriteLine("{0}は、キャノン砲を撃った!", name);
+        }
+    }
+}
