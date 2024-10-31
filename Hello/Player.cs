b4f59c1a47a63aa5a1b8320ccd0800dcfbcaf314
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-10-31 14:35:06 +0900

    プロジェクト ファイルを追加します。

---
+++ Hello/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Player
+    {
+        //フィールド
+        private string name;
+        private int level;
+
+        //コンストラクタ
+        public Player(string name,int level)
+        {
+            this.name = name;
+            this.level = level;
+        }
+        //攻撃
+        private void Attack()
+        {
+            Console.WriteLine("攻撃!!");
+        }
+        //防御
+        public void Defence()
+        {
+            Console.WriteLine("防御した");
+        }
+        //レベルアップ
+        public void LevelUp()
+        {
+            level++;
+        }
+        //名前を聞く
+        public string GetName()
+        {
+            return name;
+        }
+        //レベルを聞く
+        public int GetLevel()
+        {
+            return level;
+        }
+    }
+
+}
---
+++ Hello/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("ロト",1);
+
+            //提出課題
+            //player.level = 99999;
+            Console.WriteLine("名前 : {0}",player.GetName());
+            Console.WriteLine("レベル : {0}", player.GetLevel());
+
+            player.Attack();
+            player.Defence();
+
+            player.LevelUp();
+            Console.WriteLine("レベル : {0}", player.GetLevel());
+            //一時停止
+            Console.ReadLine();
+        }
+        
+    }
+}
